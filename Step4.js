14d8b24d220365a7d285483082961e2b82f0c1a0
duogbachdev <contact.duogbachdev@gmail.com>
2023-07-25 20:09:52 +0700

    [#3] Add Step 4

---
+++ Step4.js
@@ -0,0 +1,11 @@
+console.log("Hello Step4");
+
+// ### Step 4 : Push the code to github
+
+// _- For the repository that you own, just push straight to the main branch or the branch you switched to the new branch_
+
+// ```
+// git push origin "Branch_Name"
+// ```
+
+// _- Create a pull request & compare_
