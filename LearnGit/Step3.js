eaa9c5b06788db8154478eb90878cc9374bab17e
duogbachdev <contact.duogbachdev@gmail.com>
2023-07-25 20:07:13 +0700

    [#3] Add Step 3

---
+++ LearnGit/Step3.js
@@ -0,0 +1,15 @@
+console.log("Hello Step3");
+
+// _- Add features or modify the code in the project_
+
+// _- Add all new code_
+
+// ```
+// git add .
+// ```
+
+// _- Generate commit code_
+
+// ```
+// git commit -m "First_Commit"
+// ```
