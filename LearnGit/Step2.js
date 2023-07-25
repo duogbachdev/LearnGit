a10688ebfaed29d9f85c6b5f2503737100b52626
duogbachdev <contact.duogbachdev@gmail.com>
2023-07-25 17:13:12 +0700

    [#2] Add Step 2

--- LearnGit/Step2.js
+++ LearnGit/Step2.js
@@ -1,1 +1,9 @@
-console.log("Hello Step2");
+console.log("Hello Steo2");
+
+// ### Step 2 : Checkout new branch
+
+// _- Stand from master branch and then switch to new working branch_
+
+// ```
+// git checkout -b "New_Working_Branch"
+// ```
